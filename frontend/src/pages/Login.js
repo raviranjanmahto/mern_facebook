a0f831c33741576d300dadcfb4a158361b33cb6a
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-01-14 00:35:46 +0530

    added media queries and some changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -26,7 +26,10 @@ const Login = () => {
       .required(`Email address is required`)
       .email(`Must be a valid email`)
       .max(70, "Email is too long"),
-    password: Yup.string().required(`Password is required`),
+    password: Yup.string()
+      .required(`Password is required`)
+      .min(6, "Password must be atleast 6 character !")
+      .max(40, "Password is too long!"),
   });
 
   return (
@@ -36,7 +39,7 @@ const Login = () => {
           <div className='login_1'>
             <img src='../../icons/facebook.svg' alt='' />
             <span>
-              facebook helps you connect and share with the people in your life.
+              Facebook helps you connect and share with the people in your life.
             </span>
           </div>
           <div className='login_2'>
@@ -73,13 +76,13 @@ const Login = () => {
                 )}
               </Formik>
               <Link to='/forgot' className='forgot_password'>
-                Forgotten password ?
+                Forgotten password?
               </Link>
               <div className='sign_splitter'></div>
               <button className='blue_btn open_signup'>Create Account</button>
             </div>
             <Link to='/' className='sign_extra'>
-              <b>Create a page</b> for a celebrity, brand or business.
+              <b>Create a Page</b> for a celebrity, brand or business.
             </Link>
           </div>
         </div>
