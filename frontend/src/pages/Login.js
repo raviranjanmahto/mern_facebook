065c4c9b86ce5877dd21432293b92948c6728bae
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-01-23 22:07:50 +0530

    login frontend to backend axios

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,14 @@
 import "./Login.css";
 import { LoginForm, RegisterForm, Footer } from "../components/login";
+import { useState } from "react";
 
 const Login = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className='login'>
       <div className='login_wrapper'>
-        <LoginForm />
-        <RegisterForm />
+        <LoginForm setVisible={setVisible} />
+        {visible && <RegisterForm setVisible={setVisible} />}
         <Footer />
       </div>
     </div>
