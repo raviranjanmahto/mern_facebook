8b88598a41db75fe70498e2b8ea2563d49d3a189
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-01-23 17:48:56 +0530

    added mediaQueries

--- frontend/src/components/login/GenderSelect.js
+++ frontend/src/components/login/GenderSelect.js
@@ -1,6 +1,20 @@
+import { useMediaQuery } from "react-responsive";
+
 const GenderSelect = ({ handleChange, genderError }) => {
+  const view1 = useMediaQuery({
+    query: "(min-width:539px)",
+  });
+  const view2 = useMediaQuery({
+    query: "(min-width:850px)",
+  });
+  const view3 = useMediaQuery({
+    query: "(min-width:1170px)",
+  });
   return (
-    <div className='reg_grid'>
+    <div
+      className='reg_grid'
+      style={{ marginBottom: `${genderError && !view3 ? "70px" : "0"}` }}
+    >
       <label htmlFor='male'>
         Male
         <input
@@ -31,7 +45,18 @@ const GenderSelect = ({ handleChange, genderError }) => {
           onChange={handleChange}
         />
       </label>
-      {genderError && <div className='input_error'>{genderError}</div>}
+      {genderError && (
+        <div
+          className={
+            !view3 ? "input_error" : "input_error input_error_select_large"
+          }
+        >
+          <div
+            className={!view3 ? "error_arrow_bottom" : "error_arrow_left"}
+          ></div>
+          {genderError}
+        </div>
+      )}
     </div>
   );
 };
