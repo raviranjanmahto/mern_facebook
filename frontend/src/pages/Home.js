2996bcaa54d368d18244591444f95645886f6733
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-01-24 18:47:36 +0530

    called header inside home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/header/Header";
 
 const Home = () => {
-  return <div>Home from frontend!!!</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
