631e24c1c57b1dcf38961c6f8489128af36d7ac6
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-02 21:01:26 +0530

    added all menu entertainment shopping personal professioal commuity  meta field

--- frontend/src/components/header/AllMenu.js
+++ frontend/src/components/header/AllMenu.js
@@ -23,6 +23,83 @@ const AllMenu = () => {
                 />
               ))}
             </div>
+            <div className='all_menu_group'>
+              <div className='all_menu_group_header'>Entertainment</div>
+              {menu.slice(6, 9).map((item, i) => (
+                <AllMenuItem
+                  key={i}
+                  name={item.name}
+                  description={item.description}
+                  icon={item.icon}
+                />
+              ))}
+            </div>
+            <div className='all_menu_group'>
+              <div className='all_menu_group_header'>Shopping</div>
+              {menu.slice(9, 11).map((item, i) => (
+                <AllMenuItem
+                  key={i}
+                  name={item.name}
+                  description={item.description}
+                  icon={item.icon}
+                />
+              ))}
+            </div>
+            <div className='all_menu_group'>
+              <div className='all_menu_group_header'>Personal</div>
+              {menu.slice(11, 15).map((item, i) => (
+                <AllMenuItem
+                  key={i}
+                  name={item.name}
+                  description={item.description}
+                  icon={item.icon}
+                />
+              ))}
+            </div>
+            <div className='all_menu_group'>
+              <div className='all_menu_group_header'>Professional</div>
+              {menu.slice(15, 17).map((item, i) => (
+                <AllMenuItem
+                  key={i}
+                  name={item.name}
+                  description={item.description}
+                  icon={item.icon}
+                />
+              ))}
+            </div>
+            <div className='all_menu_group'>
+              <div className='all_menu_group_header'>Community Resources</div>
+              {menu.slice(17, 21).map((item, i) => (
+                <AllMenuItem
+                  key={i}
+                  name={item.name}
+                  description={item.description}
+                  icon={item.icon}
+                />
+              ))}
+            </div>
+            <div className='all_menu_group'>
+              <div className='all_menu_group_header'>More from Meta</div>
+              {menu.slice(21, 23).map((item, i) => (
+                <AllMenuItem
+                  key={i}
+                  name={item.name}
+                  description={item.description}
+                  icon={item.icon}
+                />
+              ))}
+            </div>
+          </div>
+          <div className='all_right'>
+            <div className='all_right_header'>Create</div>
+            {create.map((item, i) => (
+              <div className='all_right_item hover1' key={i}>
+                <div className='all_right_circle'>
+                  <i className={item.icon}></i>
+                </div>
+                {item.name}
+              </div>
+            ))}
           </div>
         </div>
       </div>
