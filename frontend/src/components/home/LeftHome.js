f3d595d481932e0121d191c7eb3f6ce84bad3838
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-05 01:22:54 +0530

    added more left home item show more field and show based on usestate

--- frontend/src/components/home/LeftHome.js
+++ frontend/src/components/home/LeftHome.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LeftHome.css";
 import LeftLink from "./LeftLink";
 import { left } from "../../data/home";
 import { Link } from "react-router-dom";
+import { ArrowDown1 } from "../../svg";
+import Shortcut from "./Shortcut";
 
 const LeftHome = ({ user }) => {
+  const [visible, setVisible] = useState(false);
   return (
-    <div className='left_home '>
+    <div className='left_home scrollbar'>
       <Link to='/profile' className='left_link hover1'>
         <img src={user?.picture} alt='' />
         <span>
@@ -21,6 +24,49 @@ const LeftHome = ({ user }) => {
           notification={link.notification}
         />
       ))}
+      {!visible && (
+        <div className='left_link hover1' onClick={() => setVisible(true)}>
+          <div className='small_circle'>
+            <ArrowDown1 />
+          </div>
+          <span>See more</span>
+        </div>
+      )}
+      {visible && (
+        <div className='more_left'>
+          {left.slice(8, left.length).map((link, i) => (
+            <LeftLink
+              key={i}
+              img={link.img}
+              text={link.text}
+              notification={link.notification}
+            />
+          ))}
+          <div className='left_link hover1' onClick={() => setVisible(false)}>
+            <div className='small_circle rotate360'>
+              <ArrowDown1 />
+            </div>
+            <span>Show less</span>
+          </div>
+        </div>
+      )}
+      <div className='splitter'></div>
+      <div className='shortcut'>
+        <div className='heading'>Your Shortcuts</div>
+        <div className='edit_shortcut'>Edit</div>
+      </div>
+      <div className='shortcut_list'>
+        <Shortcut
+          link='https://vikreta.in/'
+          img='../../images/ytb.png'
+          name='My YouTube channel'
+        />
+        <Shortcut
+          link='https://www.instagram.com/mr_expert.exe/'
+          img='../../images/insta.png'
+          name='My Instagram channel'
+        />
+      </div>
     </div>
   );
 };
