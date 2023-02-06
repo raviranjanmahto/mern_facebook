780ef70afdba52dcd886d510a55107b093f7fb25
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-07 00:00:31 +0530

    hover class added

--- frontend/src/components/home/RightHome.js
+++ frontend/src/components/home/RightHome.js
@@ -1,7 +1,8 @@
 import { Dots, NewRoom, Search } from "../../svg";
+import Contact from "./Contact";
 import "./RightHome.css";
 
-const RightHome = () => {
+const RightHome = ({ user }) => {
   const color = "#65676b";
 
   return (
@@ -12,18 +13,20 @@ const RightHome = () => {
         <div className='contacts_header'>
           <div className='contacts_header_left'>Contacts</div>
           <div className='contacts_header_right'>
-            <div className='contact_circle'>
+            <div className='contact_circle hover1'>
               <NewRoom color={color} />
             </div>
-            <div className='contact_circle'>
+            <div className='contact_circle hover1'>
               <Search color={color} />
             </div>
-            <div className='contact_circle'>
+            <div className='contact_circle hover1'>
               <Dots color={color} />
             </div>
           </div>
         </div>
-        <div className='contact_list'></div>
+        <div className='contact_list'>
+          <Contact user={user} />
+        </div>
       </div>
     </div>
   );
