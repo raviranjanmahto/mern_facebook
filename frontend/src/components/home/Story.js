f0082bae48f09ce28bd608128eeb4df9f3a474e0
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-07 23:59:11 +0530

    story component added

---
+++ frontend/src/components/home/Story.js
@@ -0,0 +1,13 @@
+const Story = ({ story }) => {
+  return (
+    <div className='story'>
+      <img src={story.image} alt='' className='story_img' />
+      <div className='story_profile_pic'>
+        <img src={story.profile_picture} alt='story_images' />
+      </div>
+      <div className='story_profile_name'>{story.profile_name}</div>
+    </div>
+  );
+};
+
+export default Story;
