ab2bd3b7c491e97e26aed0f3e558a24bd9130061
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-08 20:06:59 +0530

    added stories n right arroe button

--- frontend/src/components/home/Stories.js
+++ frontend/src/components/home/Stories.js
@@ -1,5 +1,7 @@
-import { Plus } from "../../svg";
+import { stories } from "../../data/home";
+import { ArrowRight, Plus } from "../../svg";
 import "./Stories.css";
+import Story from "./Story";
 
 const Stories = () => {
   return (
@@ -14,6 +16,12 @@ const Stories = () => {
           <Plus color='#fff' />
         </div>
         <div className='story_create_text'>Create Story</div>
+      </div>
+      {stories.map((story, i) => (
+        <Story key={i} story={story} />
+      ))}
+      <div className='white_circle'>
+        <ArrowRight color='#65676b' />
       </div>
     </div>
   );
