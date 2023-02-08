1d33552afde9d0d5e4c6d8d400bab63898eb2a05
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-09 03:10:21 +0530

    query added for react-responsiveness

--- frontend/src/components/home/Stories.js
+++ frontend/src/components/home/Stories.js
@@ -2,8 +2,22 @@ import { stories } from "../../data/home";
 import { ArrowRight, Plus } from "../../svg";
 import "./Stories.css";
 import Story from "./Story";
+import { useMediaQuery } from "react-responsive";
 
 const Stories = () => {
+  const query1175px = useMediaQuery({ query: "(max-width:1175px)" });
+  const query1030px = useMediaQuery({ query: "(max-width:1030px)" });
+  const query950px = useMediaQuery({ query: "(max-width:950px)" });
+  const query875px = useMediaQuery({ query: "(max-width:875px)" });
+  const max = query875px
+    ? 5
+    : query950px
+    ? 4
+    : query1030px
+    ? 5
+    : query1175px
+    ? 4
+    : stories.length;
   return (
     <div className='stories'>
       <div className='create_story_card'>
@@ -17,7 +31,7 @@ const Stories = () => {
         </div>
         <div className='story_create_text'>Create Story</div>
       </div>
-      {stories.map((story, i) => (
+      {stories.slice(0, max).map((story, i) => (
         <Story key={i} story={story} />
       ))}
       <div className='white_circle'>
