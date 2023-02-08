56e190bdff758746dda8890e2a5e4a48a8514589
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-08 20:09:32 +0530

    added createpost component in home page

---
+++ frontend/src/components/createPost/CreatePost.js
@@ -0,0 +1,30 @@
+import { Feeling, LiveVideo, Photo } from "../../svg";
+import "./CreatePost.css";
+
+const CreatePost = ({ user }) => {
+  return (
+    <div className='createPost'>
+      <div className='createPost_header'>
+        <img src={user?.picture} alt='user_picture' />
+        <div className='open_post'>What's on your mind, {user?.first_name}</div>
+      </div>
+      <div className='create_splitter'></div>
+      <div className='createPost_body'>
+        <div className='createPost_icon hover1'>
+          <LiveVideo color='#f3425f' />
+          Live Video
+        </div>
+        <div className='createPost_icon hover1'>
+          <Photo color='#4bbf67' />
+          Photo/Video
+        </div>
+        <div className='createPost_icon hover1'>
+          <Feeling color='#f7b928' />
+          Feeling/Activity
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
