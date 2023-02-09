83491d4d4c78ad7730a320e359a2fe3783393720
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-09 22:05:16 +0530

    added verify case n updating verified=true in cookies frontend

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -7,6 +7,8 @@ export function userReducer(
   switch (action.type) {
     case "LOGIN":
       return action.payload;
+    case "VERIFY":
+      return { ...state, verified: action.payload };
 
     default:
       return state;
