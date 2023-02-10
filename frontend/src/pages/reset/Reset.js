0afd54e61183a54a3138bbe6232d65eac07e93b4
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-11 02:06:03 +0530

    responsiveness n code refactor

--- frontend/src/pages/reset/Reset.js
+++ frontend/src/pages/reset/Reset.js
@@ -1,15 +1,15 @@
-import { Form, Formik } from "formik";
 import Cookies from "js-cookie";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import LoginInput from "../../components/inputs/LoginInput";
 import "./Reset.css";
+import SearchAccount from "./SearchAccount";
 
 const Reset = () => {
   const { user } = useSelector(state => ({ ...state }));
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [visible, setVisible] = useState(0);
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
   const logout = () => {
@@ -44,34 +44,9 @@ const Reset = () => {
         )}
       </div>
       <div className='reset_wrap'>
-        <div className='reset_form'>
-          <div className='reset_form_header'>Find Your Account</div>
-          <div className='reset_form_text'>
-            Please enter your email address or mobile number to search for your
-            account.
-          </div>
-          <Formik enableReinitialize initialValues={{ email }}>
-            {formik => (
-              <Form>
-                <LoginInput
-                  type='text'
-                  name='email'
-                  onChange={e => setEmail(e.target.value)}
-                  placeholder='Email address or phone number'
-                />
-                {error && <div className='error_text'>{error}</div>}
-                <div className='reset_form_btn'>
-                  <Link to='/' className='grey_btn'>
-                    Cancel
-                  </Link>
-                  <button type='submit' className='blue_btn'>
-                    Search
-                  </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
+        {visible === 0 && (
+          <SearchAccount email={email} setEmail={setEmail} error={error} />
+        )}
       </div>
     </div>
   );
---
+++ frontend/src/pages/reset/SearchAccount.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import LoginInput from "../../components/inputs/LoginInput";
+import { Link } from "react-router-dom";
+
+const SearchAccount = ({ email, setEmail, error }) => {
+  return (
+    <div className='reset_form'>
+      <div className='reset_form_header'>Find Your Account</div>
+      <div className='reset_form_text'>
+        Please enter your email address or mobile number to search for your
+        account.
+      </div>
+      <Formik enableReinitialize initialValues={{ email }}>
+        {formik => (
+          <Form>
+            <LoginInput
+              type='text'
+              name='email'
+              onChange={e => setEmail(e.target.value)}
+              placeholder='Email address or phone number'
+            />
+            {error && <div className='error_text'>{error}</div>}
+            <div className='reset_form_btn'>
+              <Link to='/' className='grey_btn'>
+                Cancel
+              </Link>
+              <button type='submit' className='blue_btn'>
+                Search
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default SearchAccount;
