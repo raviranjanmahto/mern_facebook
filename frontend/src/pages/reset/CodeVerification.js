a6541fc033efc33b2969cf321cffb6c0b2e7aa69
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-12 02:45:05 +0530

    code validation added

--- frontend/src/pages/reset/CodeVerification.js
+++ frontend/src/pages/reset/CodeVerification.js
@@ -2,15 +2,26 @@ import React from "react";
 import { Form, Formik } from "formik";
 import LoginInput from "../../components/inputs/LoginInput";
 import { Link } from "react-router-dom";
+import * as Yup from "yup";
 
 const CodeVerification = ({ code, setCode, error }) => {
+  const validateCode = Yup.object({
+    code: Yup.string()
+      .required("Code is required!")
+      .min(6, "Code must be 6 character")
+      .max(6, "Code must be 6 character"),
+  });
   return (
     <div className='reset_form'>
       <div className='reset_form_header'>Code Verification</div>
       <div className='reset_form_text'>
         Please enter code that has been sent to your email.
       </div>
-      <Formik enableReinitialize initialValues={{ code }}>
+      <Formik
+        enableReinitialize
+        initialValues={{ code }}
+        validationSchema={validateCode}
+      >
         {formik => (
           <Form>
             <LoginInput
