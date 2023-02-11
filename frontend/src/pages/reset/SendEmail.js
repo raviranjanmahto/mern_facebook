eff4b61719c0f84be671aa2072bd5a62d0ec0ba5
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-11 15:58:26 +0530

    added sendMail page frontend

---
+++ frontend/src/pages/reset/SendEmail.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SendEmail = ({ user }) => {
+  return (
+    <div className='reset_form dynamic_height'>
+      <div className='reset_form_header'>Reset Your Password</div>
+      <div className='reset_grid'>
+        <div className='reset_left'>
+          <div className='reset_form_text'>
+            How do you want to receive the code to reset your password?
+          </div>
+          <label htmlFor='email' className='hover1'>
+            <input type='radio' id='email' checked readOnly />
+            <div className='label_col'>
+              <span>Send code via email</span>
+              <span>exaple@gmail.com</span>
+            </div>
+          </label>
+        </div>
+        <div className='reset_right'>
+          <img src={user?.picture} alt='' />
+          <span>exaple@gmail.com</span>
+          <span>Facebook user</span>
+        </div>
+      </div>
+      <div className='reset_form_btn'>
+        <Link to='/' className='grey_btn'>
+          Not You ?
+        </Link>
+        <button type='submit' className='blue_btn'>
+          Continue
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SendEmail;
